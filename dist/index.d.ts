fc18ce44f3a24396b55d565423220b0f4262c359
ilikdoge <ilikdoge0@gmail.com>
2023-07-20 19:37:19 -0700

    Published branch "master"

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+declare const native: NativeFunctions;
+interface NativeFunctions {
+    randombytes_buf(buf: Buffer | Uint8Array): void;
+    crypto_secretbox_easy(out: Buffer | Uint8Array, inp: Buffer | Uint8Array, len: number, nonce: Buffer | Uint8Array, key: Buffer | Uint8Array): void;
+}
+
+export { native as default };
