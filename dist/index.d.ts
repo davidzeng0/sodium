46cba0637db00b550683a1c97fdf82d898b3aa26
ilikdoge <ilikdoge0@gmail.com>
2023-07-20 19:51:35 -0700

    Published branch "master"

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
 declare const native: NativeFunctions;
 interface NativeFunctions {
     randombytes_buf(buf: Buffer | Uint8Array): void;
-    crypto_secretbox_easy(out: Buffer | Uint8Array, inp: Buffer | Uint8Array, len: number, nonce: Buffer | Uint8Array, key: Buffer | Uint8Array): void;
+    crypto_secretbox_easy(out: Buffer | Uint8Array, inp: Buffer | Uint8Array, nonce: Buffer | Uint8Array, key: Buffer | Uint8Array): void;
 }
 
 export { native as default };
--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 'use strict';
 
-var t=(r=>typeof require<"u"?require:typeof Proxy<"u"?new Proxy(r,{get:(e,n)=>(typeof require<"u"?require:e)[n]}):r)(function(r){if(typeof require<"u")return require.apply(this,arguments);throw Error('Dynamic require of "'+r+'" is not supported')});var i=t("bindings")("nsodium"),u=i;
+var n=(r=>typeof require<"u"?require:typeof Proxy<"u"?new Proxy(r,{get:(e,t)=>(typeof require<"u"?require:e)[t]}):r)(function(r){if(typeof require<"u")return require.apply(this,arguments);throw Error('Dynamic require of "'+r+'" is not supported')});var i=n("bindings")("nsodium"),a=i;
 
-module.exports = u;
+module.exports = a;
