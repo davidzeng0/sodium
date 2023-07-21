5ca9a45953f3dd273988244953186832a2e6a89f
ilikdoge <ilikdoge0@gmail.com>
2023-07-20 20:10:20 -0700

    return buffer length

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 const native = require('bindings')('nsodium') as NativeFunctions;
 
 interface NativeFunctions{
-	randombytes_buf(buf: Buffer | Uint8Array): void;
-	crypto_secretbox_easy(out: Buffer | Uint8Array, inp: Buffer | Uint8Array, nonce: Buffer | Uint8Array, key: Buffer | Uint8Array): void;
+	randombytes_buf(buf: Buffer | Uint8Array): number;
+	crypto_secretbox_easy(out: Buffer | Uint8Array, inp: Buffer | Uint8Array, nonce: Buffer | Uint8Array, key: Buffer | Uint8Array): number;
 }
 
 export default native;
